a45f1c008d334655d04b2557a3d78a2ddf5b81d6
Delsin7 <65052355+Delsin7@users.noreply.github.com>
2020-05-08 18:09:51 -0700

    Add files via upload

---
+++ skill Grudge.js
@@ -0,0 +1,108 @@
+/*//=========Grudge Skill by Delsin7=====================================================================
+//
+//Passive Action _getSurvivalValue function pointer info from 1-239's Notkill plugin, everything else done by Delsin7.
+//
+//Grudge is a type of limited Survival skill-
+//Upon receiving lethal damage, if the user can counter it sets HP to 1 so can counter attack, however the unit dies at end of battle.
+//It also increases damage dealt by 25%. To use a custom damage percent, add the custom param  grudgeDamage: n   ,where n is any whole number.
+//An n of 100 is 100% extra damage. If you set n to 0, there will be no bonus damage.
+//
+//
+//Use custom keyword 'Grudge' on a custom type skill to set up.
+//
+*///=====================================================================================================
+
+(function() {
+
+var alias1grudge = SkillRandomizer.isCustomSkillInvokedInternal;
+SkillRandomizer.isCustomSkillInvokedInternal = function(active, passive, skill, keyword) {
+	if (keyword === 'Grudge') {
+		return this._isSkillInvokedInternal(active, passive, skill);
+	}
+	
+	return alias1grudge.call(this, active, passive, skill, keyword);
+};
+
+var alias2grudge = AttackEvaluator.PassiveAction._getSurvivalValue;
+AttackEvaluator.PassiveAction._getSurvivalValue = function(virtualActive, virtualPassive, attackEntry) {
+
+	var key = alias2grudge.call(this, virtualActive, virtualPassive, attackEntry);
+	var active = virtualActive.unitSelf;
+	var passive = virtualPassive.unitSelf;
+	aCount = virtualPassive.roundCount;
+
+	if(aCount > 0){//Ensure user can counter before activating skill check, since dies at end, don't need to activate if can't counter anyway
+		skill = SkillControl.checkAndPushCustomSkill(passive, active, attackEntry, false, 'Grudge');
+	
+		if (skill !== null) {
+			virtualPassive.unitSelf.custom.grudge = true;
+			return skill.getSkillValue();
+		}
+	}
+	return key;
+};
+
+var alias2090grudge = AttackEvaluator.HitCritical.calculateDamage;
+AttackEvaluator.HitCritical.calculateDamage = function(virtualActive, virtualPassive, entry) {//moving to multihit system since it causes issues with being accessed before
+	var damage = alias2090grudge.call(this, virtualActive, virtualPassive, entry);
+	var active = virtualActive.unitSelf;
+	var passive = virtualPassive.unitSelf;
+	//root.log(active.getName());
+	if(active.custom.grudge){
+		skill = SkillControl.getPossessionCustomSkill(active,'Grudge');
+		if(skill && skill.custom.grudgeDamage){
+			n = 1 + (skill.custom.grudgeDamage * 0.01);
+			damage = Math.round(damage * n);
+		}else{
+			damage = Math.round(damage * 1.25);
+		}
+
+	}
+	return damage;
+};
+
+var alias2091grudge = RealBattle.endBattle;//reason it wasn't working was because still had BaseBattle instead of RealBattle >.>
+RealBattle.endBattle = function() {
+	
+	var battlerActive = this.getActiveBattler();
+	var battlerPassive = this.getPassiveBattler();
+
+	if(battlerActive.getUnit().custom.grudge){
+		//root.log('active has grudge');
+		delete battlerActive.getUnit().custom.grudge;
+		//battlerActive.getUnit().setHp(0);//0 doesn't update, higher amounts work, find another kill solution
+		//this._uiBattleLayout.setDamage(battlerActive, 1, false, false);
+		DamageControl.setDeathState(battlerActive.getUnit());//to get it to kill unit visually would prolly need a check somewhere else
+		//1000,
+		//sound = root.createResourceHandle(true, 1000, 0, 0, 0);
+		//MediaControl.soundPlay(sound);
+		//DamageControl.reduceHp(battlerActive.getUnit(),1);
+		//battlerActive.lostBattler();
+		//battlerActive.setColorAlpha(alpha);
+		//root.log(battlerActive.getUnit().custom.grudge);
+	}
+	if(battlerPassive.getUnit().custom.grudge){
+		delete battlerPassive.getUnit().custom.grudge;
+		DamageControl.setDeathState(battlerPassive.getUnit());
+	}
+
+	alias2091grudge.call(this);
+};
+
+var alias2092grudge = EasyBattle.endBattle;
+EasyBattle.endBattle = function() {
+	
+	var battlerActive = this.getActiveBattler();
+	var battlerPassive = this.getPassiveBattler();
+	if(battlerActive.getUnit().custom.grudge){
+		delete battlerActive.getUnit().custom.grudge;
+		DamageControl.setDeathState(battlerActive.getUnit());
+	}
+	if(battlerPassive.getUnit().custom.grudge){
+		delete battlerPassive.getUnit().custom.grudge;
+		DamageControl.setDeathState(battlerPassive.getUnit());
+	}
+	alias2092grudge.call(this);
+};
+
+})();
